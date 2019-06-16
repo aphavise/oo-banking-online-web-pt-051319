f809be4f4a283fec4da3213ab13537fb4ca87bbc
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-16 05:57:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,12 +9,8 @@ class Transfer
     @status = "pending"
   end
 
-  def both_valid?
-    if sender.valid? && receiver.valid?
-      true
-    else
-      false
-    end
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
   end
 
   def execute_transaction
